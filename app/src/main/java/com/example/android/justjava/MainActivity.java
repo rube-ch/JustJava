5c7be867c06e6f3598e6a6081f2e9a544c6b8403
Ruben Cornejo <sistemas@usa.edu.mx>
2015-07-13 11:57:15 -0500

    Botonos + y -, sin variables

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -28,6 +28,22 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
+    /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
